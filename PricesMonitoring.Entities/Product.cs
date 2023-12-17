4897c069efdf448f844c060c8d27934882f73b54
oskolkovlad <oskolkovlad@yandex.ru>
2023-12-17 16:35:55 +0300

    Добавил сущности.

---
+++ PricesMonitoring.Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PricesMonitoring.Entities
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public string? Link { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public decimal? DiscountedPrice { get; set; }
+
+        [Required]
+        public Shop Shop { get; set; } = null!;
+    }
+}
---
+++ PricesMonitoring.Entities/Shop.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PricesMonitoring.Entities
+{
+    public class Shop
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Code { get; set; } = null!;
+    }
+}
